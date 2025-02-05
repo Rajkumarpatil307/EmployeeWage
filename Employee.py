56eead1e6e4d748615ed916533e63874694faa01
Rajkumarpatil307 <patil.rajkumar307@gmail.com>
2025-02-05 15:55:30 +0530

    UC 6 Calculate Wages till a condition of total working hours or days is reached for a month

--- Employee.py
+++ Employee.py
@@ -1,35 +1,33 @@
 import random
 
-def calculate_daily_wage(wage_per_hour):
-    attendance = random.randint(0, 2)
-    
-    match attendance:
-        case 1:
-            daily_wage = wage_per_hour * 8
-            print(f"Employee is Present (Full-time). Daily Wage: {daily_wage}")
-        case 2:
-            daily_wage = wage_per_hour * 4
-            print(f"Employee is Present (Part-time). Daily Wage: {daily_wage}")
-        case _:
-            print("Employee is Absent. Daily Wage: 0")
-
 def calculate_monthly_wage(wage_per_hour):
+  def calculate_wages_until_condition(wage_per_hour):
     total_wage = 0
     working_days = 20
+    total_hours = 0
+    working_days = 0
 
     for _ in range(working_days):
+      while total_hours < 100 and working_days < 20:
         attendance = random.randint(0, 2)
-        
+
         match attendance:
             case 1:
                 total_wage += wage_per_hour * 8
+                total_hours += 8
+                working_days += 1
             case 2:
                 total_wage += wage_per_hour * 4
+                total_hours += 4
+                working_days += 1
             case _:
                 total_wage += 0
-    
-    print(f"Total Monthly Wage: {total_wage}")
+                pass
 
-# Function calls
-calculate_daily_wage(20)
+    print(f"Total Monthly Wage: {total_wage}")
+    print(f"Total Wage: {total_wage}")
+    print(f"Total Hours Worked: {total_hours}")
+    print(f"Total Days Worked: {working_days}")
+
 calculate_monthly_wage(20)
+calculate_wages_until_condition(20)
